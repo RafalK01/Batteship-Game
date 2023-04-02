27499c53cc4bd0378710abd0038ebf743552dfb9
Rafal Kolibaba <rafal.kolibaba@icloud.com>
2023-04-02 16:58:54 +0200

    create logic for computer placing ships randomly on the board

--- game.js
+++ game.js
@@ -2,20 +2,12 @@
 const playersBoard = document.querySelector(".players-board")
 const computersBoard = document.querySelector(".computers-board")
 const flipButton = document.querySelector(".flip-button")
-
-
 const allShips = document.querySelectorAll(".ship-dock > div")
-const carrier = document.querySelector(".carrier")
-const battleship = document.querySelector(".battleship")
-const cruiser = document.querySelector(".cruiser")
-const submarine = document.querySelector(".submarine")
-const destroyer = document.querySelector(".destroyer")
-
 
 // puts 100 game fielsd into each board
 for (let i = 1; i <= 100; i++) {
-    playersBoard.innerHTML += `<div class="${i}"></div>`
-    computersBoard.innerHTML += `<div class="${i}"></div>`
+    playersBoard.innerHTML += `<div id=${i} class=""></div>`
+    computersBoard.innerHTML += `<div id=${i} class=""></div>`
 }
 
 // flip ships in the doc function
@@ -26,77 +18,90 @@ flipButton.addEventListener("click", click => {
 allShips.forEach(ship => {
     ship.style.transform = "none"
 })
+
 function flipShip() {
     allShips.forEach(ship => {
         ship.style.transform === "none" ? ship.style.transform = "rotate(90deg)" : ship.style.transform = "none"
     })
 }
 
+//CREATE A SHIP FOR COMPUTER
+class Ship {
+    constructor (name, length){
+        this.name = name,
+        this.length =length
+    }
+}
+const carrier = new Ship("carrier", 6)
+const battleship = new Ship("battleship", 5)
+const cruiser = new Ship("cruiser", 4)
+const submarine = new Ship("submarine", 3)
+const destroyer = new Ship("destroyer", 2)
 
+const shipsArray = [carrier, battleship, cruiser, submarine, destroyer]
 
-////DRAG
-
-function placeShipOnBoard(battleship){ 
-let shipInitialX = 0
-let shipInitialY = 0
-let movingShipX = 0
-let movingShipY = 0
-let isDragging = false;
-
-allShips.forEach(ship => {
-    ship.addEventListener("mousedown",dragStart)
-})
-
-function dragStart(event){
-
-    shipInitialX = event.clientX - movingShipX;
-    shipInitialY = event.clientY - movingShipY;
-
-    if (event.target === battleship) {
-        isDragging = true;
-        document.addEventListener('mousemove', dragProcess);
-        document.addEventListener('mouseup', dragStop);
+// COPMUTER PLACE SHIPS RANDOMLY
+function computerPlaceShip(ship) {
+    const computersBoardBlocks = document.querySelectorAll(".computers-board div");
+    const randomBoolean = Math.random() < 0.5;
+    const isHorizontal = randomBoolean;
+    let randomStartIndex = Math.floor(Math.random() * 100);
+  
+    let goodStartIndex;
+    if (isHorizontal) {
+      // Calculate goodStartIndex for horizontal placement
+      goodStartIndex = randomStartIndex <= 100 - ship.length ? randomStartIndex : 100 - ship.length;
+    } else {
+      // Calculate goodStartIndex for vertical placement
+      goodStartIndex = randomStartIndex <= 100 - 10 * ship.length ? randomStartIndex : randomStartIndex % 10;
+    }
+  
+    // create array of board blocks busy with computer's ships
+    const shipBlocks = [];
+    for (let i = 0; i < ship.length; i++) {
+      if (isHorizontal) {
+        shipBlocks.push(computersBoardBlocks[Number(goodStartIndex) + i]);
+      } else {
+        shipBlocks.push(computersBoardBlocks[Number(goodStartIndex) + i * 10]);
       }
+    }
+  
+    // prevent overflowing of computer's ships  
+    let noOverflow = true;
+    if (isHorizontal) {
+      noOverflow = shipBlocks.every((_shipBlock, index) => {
+        const lastBlock = shipBlocks[shipBlocks.length - 1];
+        return lastBlock.id % 10 >= index + 1 && !lastBlock.classList.contains("busy");
+      });
+    } else {
+      noOverflow = shipBlocks.every((_shipBlock, index) => {
+        const lastBlock = shipBlocks[shipBlocks.length - 1];
+        return lastBlock.id <= 90 + (index + 1) * 10 && !lastBlock.classList.contains("busy");
+      });
+    }
+  
+    // prevent stacking ships on top of each other
+    const notBusy = shipBlocks.every(shipBlock => !shipBlock.classList.contains("busy"));
+  
+    // computer place random ships if no overflow
+    if (noOverflow && notBusy) {
+      shipBlocks.forEach(shipBlock => {
+        shipBlock.classList.add(ship.name);
+        shipBlock.classList.add("busy");
+      });
+    } else {
+      computerPlaceShip(ship);
+    }
 }
-
-function dragProcess(event) {
-
-    if (isDragging) {
-        movingShipX = event.clientX - shipInitialX;
-        movingShipY = event.clientY - shipInitialY;
-    
-        battleship.style.transform = `translate(${movingShipX}px, ${movingShipY}px)`;
-      }   
-}
-function dragStop(event){
-    shipInitialX = movingShipX;
-    shipInitialY = movingShipY;
-
-    isDragging = false;
-
-    document.removeEventListener('mousemove', dragStart);
-    document.removeEventListener('mouseup', dragStop);
-} 
-}
-allShips.forEach(ship => {
-    placeShipOnBoard(ship)
+  
+// COPMUTER PLACE EACHE SHIP ON THE BOARD
+shipsArray.forEach(ship => {
+computerPlaceShip(ship) 
 })
-//
 
 
-//DROP THE SHIP
-// const individualBoardBlock = document.querySelector(".players-board > div")
-// BELOW DOES NOT WORK
-document.querySelectorAll(".players-board > div").forEach(ship => {
-    ship.addEventListener("mouseup",dropShip)
-})
 
-function dropShip(){
-    console.log("dropped")
-}
 
-// Problems:
-// it is not possible to place flipped ships
 
 
 
