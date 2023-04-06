9631594fcb08861878f7d085c76b28b4b218fa40
Rafal Kolibaba <rafal.kolibaba@icloud.com>
2023-04-06 10:48:05 +0200

    position landing start button

--- landing.js
+++ landing.js
@@ -10,6 +10,11 @@ function setup() {
   cols = w / scl;
   rows = h / scl;
 
+  button = createButton("Let's go!");
+  button.class("go-button")
+  // button.position(width / 2 -100, 500)
+  button.mousePressed(goToNextPage);
+
 }
 
 function draw() {
@@ -43,10 +48,10 @@ function draw() {
   textAlign(CENTER, CENTER);
   text("Make' em sink!!", width / 2, height / 2);
 
-  button = createButton("Let's go!");
-  button.class("go-button")
-  button.position(width / 2 -100, 500)
-  button.mousePressed(goToNextPage);
+  // button = createButton("Let's go!");
+  // button.class("go-button")
+  // // button.position(width / 2 -100, 500)
+  // button.mousePressed(goToNextPage);
   
 }
 
